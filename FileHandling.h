5b27df0b3b337034c37c51402f09ba54c6edb615
zyuvraj53 <zyuvraj53@gmail.com>
2024-10-02 19:05:59 +0530

    Updated header file for FileHandling

--- FileHandling.h
+++ FileHandling.h
@@ -2,5 +2,21 @@
 #include <iostream>
 #include <string>
 #include <array>
+#include <unordered_map>
+
+#include "HuffmanEncoding.h"
 
 std::array<char, 128> getCount(const char *path);
+
+void delete_content(const char *path);
+
+Node *makeTree(std::array<char, 128> characters);
+
+void buildCodes_util(std::unordered_map<char, std::string> huffmanCodes, Node* root,const std::string &str);
+
+std::unordered_map<char, std::string> buildCodes(Node* root);
+
+void zip_file(std::unordered_map<char, std::string> codes, const char* in_path, const char* out_path);
+
+
+void encode_file(std::unordered_map<char, std::string> huffmanCodes, const char* in_path, const char* out_path);
